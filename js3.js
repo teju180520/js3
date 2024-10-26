9b3b4893237436017b67d8ed350503f005d78413
teju <laditejeswari912@gmail.com>
2024-10-26 11:22:12 +0530

    mass

---
+++ js3.js
@@ -0,0 +1,35 @@
+let numbers=+prompt("write your favrote number");
+let a=(numbers%2==0)? "your number is even": "your number is odd" ;
+console.log(a);
+
+
+let grade=+prompt("enter your marks get your grade");
+       let b=(grade<=45)?"F+":
+             (grade<=60)?"E+":
+             (grade<=75)?"D+":
+             (grade<=90)?"B+": "A+";  
+             
+             console.log(b);
+
+
+   let temp=+prompt("enter your celsias get your degrees");
+   let c=(temp<=10)?"cold":
+         (temp<=25)?"cool":
+         (temp<=35)?"warm":
+         (temp<=45)?"hot": "too hot";
+         console.log(c); 
+
+
+
+
+   let  age=+prompt("enter your age get your age group");
+   let d=(age<=10)?"children":
+         (age<=25)?"teenage":
+         (age<=35)?"younger":
+         (age<=55)?"senior": "senior citizen";
+         console.log(d); 
+
+
+         let voter=+prompt("enter your voter age")
+         let e=(voter<=18)?"adul":"mainar";
+         console.log(e);
